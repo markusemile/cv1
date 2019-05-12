40c54a2faeeed35c2d8574cf71ec3bb479c6984c
markus Emile <emile.markus@gmail.com>
2019-05-13 00:03:55 +0200

    12/05/2019
    jk

--- index.php
+++ index.php
@@ -4,17 +4,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <link
-        href="http://fonts.googleapis.com/css?family=Roboto:400,300italic,300,100italic,100,400italic,500,500italic,700,900,900italic,700italic%7COswald:400,300,700"
-        rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,300italic,300,100italic,100,400italic,500,500italic,700,900,900italic,700italic%7COswald:400,300,700" rel="stylesheet" type="text/css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/animate.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
     <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="css/custom.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+    <script type="text/javascript" src="https://cdn.bootcss.com/typed.js/1.1.4/typed.min.js"></script>
 
 
     <title>mon cv</title>
@@ -29,23 +27,31 @@
                 <img src="media/moi.jpg" class="img-fluid" alt="">
             </div>
             <nav class="navbar  navbar-expand-lg navbar-dark bg-dark">
-                <button class="navbar-toggler d-lg-none ml-auto" type="button" data-toggle="collapse"
-                    data-target="#myMenu" aria-controls="myMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#myMenu" aria-controls="myMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+            
+
+
+
+
+
+
+
+
                 <div class="collapse navbar-collapse" id="myMenu">
                     <ul class="nav navbar-nav mr-auto mt-2 mt-lg-0">
                         <li class="nav-item active">
-                            <a class="nav-link active bt_home" href="#">Home</a>
+                            <a class="nav-link active bt_home" href="#">Accueil</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link bt_about" href="#">About-me</a>
+                            <a class="nav-link bt_about" href="#">A propos</a>
                         </li>
                         <li class="nav-item ">
-                            <a class="nav-link bt_skill" href="#">Skills</a>
+                            <a class="nav-link bt_skill" href="#">Compétences</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link bt_expert" href="#">Experience</a>
+                            <a class="nav-link bt_expert" href="#">Experiences</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link bt_portfolio" href="#">Portfolio</a>
@@ -57,65 +63,156 @@
                 </div>
             </nav>
             <!--  fin de la navigation -->
-           
+
         </aside>
         <div id="mainpage">
-                <div class="main-container">
-                    <div class="divToSlide">
-                        <div id="section-home" class="section"></div>
-                        <div id="section-aboutMe" class="section">
-                            <div class="row section-title">                                
-                                <h2>About - Me</h2>                            
+            <div class="main-container">
+                <div class="divToSlide">
+                    <!--     SECTION HOME                 -->
+                    <div id="section-home" class="section">
+                        <div class="monMessage">
+                            <h1>Je m'appelle...Markus Emile !</h1>
+                            <div class="container typedText">
+                                <span class="text-dark">Je suis un Passionné </span><span class="slogan"></span>
                             </div>
                         </div>
-                        <div id="section-skill" class="section">
-                            <div class="row section-title">                                
-                                <h2>Skills</h2>                            
-                            </div>
+                    </div>
+                    <!--     SECTION ABOUT-ME             -->
+                    <div id="section-aboutMe" class="section">
+                        <div class="row section-title">
+                            <h2>A propos de moi...</h2>
                         </div>
-                        <div id="section-experience" class="section">
-                            <div class="col-md-12">
-                                <div class="row section-title">                                
-                                     <h2>Experience</h2>                            
+                        <div class="row col-sm-12 col-md-12">
+                            <ul>
+                                <li>Nom :<em>Markus Emile</em>
+                                </li>
+                                <li>Email :<em><a href="mailto:emile.markus@gmail.com">emile.markus@gmail.com</a></em>
+                                </li>
+                                <li>Téléphone :<em>+32 (0)483 121.096</em>
+                                </li>
+                                <li>Date de naissance :<em>29 Decembre 1972</em>
+                                </li>
+                                <li>Nationalité : <em>Belge</em>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="row pt-5 col-sm-12 col-md-6">
+                            <h4><strong>profil professionnel</strong></h4><br><br>
+                            <h5 class="text-primary">
+                                Comme vous pourrez le constater, je suis de nature autodidacte, j‘aime m‘enrichir l‘esprit et pense avoir le contact assez facile. Je m‘adapte facilement à une nouvelle équipe et/ou environnement. Resistant au stress et à la pression, habitué à devoir penser à plusieurs choses à la fois.
+
+                                </h5>
+                        
+
+
+                        </div>
+                        <div class="row pt-3 col-md 4">
+                            <span><a href="" class="button">télécharger mon C.V.</a></span>
+                        </div>
+
+
+                    </div>
+                    <div id="section-skill" class="section">
+                        <div class="row section-title">
+                            <h2>Skills</h2>
+                        </div>
+                    </div>
+                    <div id="section-experience" class="section">
+                        <div class="col-md-12">
+                            <div class="row section-title">
+                                <h2>Experience</h2>
+                            </div>
+                            <div class="row">
+                                <h4 class="text-center pt-3 pb-5">
+                                Mes secteurs de prédilection ont en commun l‘utilisation de l‘ordinateur et l‘aide à autrui dans le milieu informatique. Mais je suis ouvert à
+toute proposition que vous trouverez en adéquation avec mon profil .</h4>
+                            
+
+
+                            </div>
+                            <div class="row myjob justify-content-between">
+                                <div class="col-sm-6 col-md-6 d-block">
+                                    <div class="col-sm-6 col-md-3 float-left">
+                                        <img src="media/icone/printer.png" class="img-fluid iconeY" alt="">
+                                    </div>
+                                    <div class="col-sm-6  float-left c-d-n">
+                                        <p class="text-form-1">
+                                            <h5 class="text-success">All Printing Service</h5>
+                                        </p>
+                                        <p class="text-form-2 d-block">
+                                            <h4 class="text-primary">Printer Shop Senior</h4>
+                                        </p>
+                                        <p class="text-form-3 d-block"><span>2011-2019</span>
+                                        </p>
+                                    </div>
                                 </div>
-                                <div class="row myjob justify-content-between">                                    
-                                    <div class="col-md-3">
-                                        <p class="text-form-1 d-block"><h5 class="text-warning">All Printing Service</h5></p>
-                                        <p class="text-form-2 d-block"><h4 class="text-primary">Printer Shop Senior</h4></p>
-                                        <p class="text-form-3 d-block"><span>2011-2019</span></p>
+
+                                <div class="col-sm-12 col-md-6 desc-job">
+                                    Depuis plus de 6 ans, et encore au quotidien, j‘utilise mon savoir faire et mes connaissances acquises dans <strong>l‘informatique</strong>, <strong>le graphisme</strong> et <strong>la gestion de la clientèle</strong>, où je dois jongler entre les écrans, machines d‘impressions, téléphones et autres outils de finition. On pourrait penser que l‘on travaillle dans un monde de machines uniquement mais le contact clientèle tient une place toute aussi importante. Les machines ne sont que les outils pour arriver à satisfaire la demande de nos clients, des demandes aussi variées que de simples duplicatas de documents jusqu‘a des réalisations plus personnelles comme des flyers, cartes de voeux ou de décès.<br><br> Mon côté <strong>créatif</strong> m‘aide dans la mise en page (pré-press),de rollup , de poster ou encore la création d‘un logo pour une société. Dès lors la <strong>communication</strong> entre le client et moi-même est <strong>primordiale</strong>, que cela soit en français ou anglais. Mais rien n‘est possible sans un bon maintien de l‘outil, du commerce et des stocks, exercice dans lequel il est important de s‘impliquer pour une meilleure gestion de la demand
+                                </div>
+                            </div>
+                            <div class="row myjob justify-content-between mt-3">
+                                <div class="col-sm-6 col-md-6 d-block">
+                                    <div class="col-sm-6 col-md-3 float-left">
+                                        <img src="media/icone/gravure.png" class="img-fluid iconeY" alt="">
                                     </div>
-                                    <div class="col-md-1">
-                                        <img src="https://image.flaticon.com/icons/svg/62/62144.svg" class="img-fluid iconeY" alt="">
+                                    <div class="col-sm-6 float-left c-d-n">
+                                        <p class="text-form-1">
+                                            <h5 class="text-success">Immo-Plaquette</h5>
+                                        </p>
+                                        <p class="text-form-2 d-block">
+                                            <h4 class="text-primary">GAO<br><span class="lead">Gravure assistée par ordinateur</span></h4>
+                                        </p>
+                                        <p class="text-form-3 d-block"><span>2009-2010</span>
+                                        </p>
                                     </div>
-                                    <div class="col-md-7">
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit ipsam eaque, ducimus voluptatem voluptas cumque iusto ullam mollitia numquam quos tempore quaerat maiores maxime, cupiditate quia autem deserunt asperiores rem id. Labore ipsum, unde, dolorum fugiat accusantium ab nulla quis fuga reprehenderit molestiae eum tenetur id? Porro perspiciatis ducimus voluptas quasi veniam qui, magni provident aspernatur maiores veritatis nam animi blanditiis, laborum quas ad odio nemo consectetur, ut tempore corrupti sapiente non. Dicta quae dolore iste magni repellat obcaecati labore. Ipsam soluta architecto, dolorem ex neque earum in nobis recusandae repellendus maiores sit ut vitae illum pariatur est provident corrupti!
+                                </div>
+                                <div class="col-sm-12 col-md-6 desc-job">
+                                    Un métier que je ne connaissais pas, mais que très vite j‘appris et me permis de réaliser divers gravures sur divers supports comme le plastique, l‘aluminium ou encore la réalisation de plaque pour ambassade par fraisage en partant d‘une simple image.
+                                </div>
+                            </div>
+                            <div class="row myjob justify-content-between mt-3">
+                                <div class="col-sm-6 col-md-6 d-block">
+                                    <div class="col-sm-6 col-md-3 float-left">
+                                        <img src="media/icone/lan.png" class="img-fluid iconeY" alt="">
                                     </div>
+                                    <div class="col-sm-6 float-left">
+                                        <p class="text-form-1">
+                                            <h5 class="text-success">CF2D-CF2M</h5>
+                                        </p>
+                                        <p class="text-form-2 d-block">
+                                            <h4 class="text-primary">TECHNICIEN PC & RESEAU</h4>
+                                        </p>
+                                        <p class="text-form-3 d-block"><span>2005-2006</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12 col-md-6 desc-job">
+                                    Maintenance et reconditionnement de PC. <strong>Déploiment de système</strong> d‘exploitation (linux) en réseau, <strong>maintenance de réseau</strong>. Réalisation de courte formation sur KDE/GNOME pour employés. Installation des câbles réseaux. <strong>Réalisation</strong> d‘un <strong>petit programme php</strong> pour gérer le stock des Pc avec code barre.
                                 </div>
                             </div>
                         </div>
-                        <div id="section-portfolio" class="section">
-                            <div class="row section-title">                                
-                                <h2>Portfolio</h2>                            
-                            </div>
+                    </div>
+                    <div id="section-portfolio" class="section">
+                        <div class="row section-title">
+                            <h2>Portfolio</h2>
                         </div>
-                        <div id="section-contact" class="section">
-                            <div class="row section-title">                                
-                                <h2>Contact</h2>                            
-                            </div>
+                    </div>
+                    <div id="section-contact" class="section">
+                        <div class="row section-title">
+                            <h2>Contact</h2>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     </div>
 
 
 
 
     <script src="js/jquery.easing.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
-    <script src="js/typed.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/actions.js"></script>
 </body>
 
--- js/actions.js
+++ js/actions.js
@@ -1,14 +1,35 @@
 $(function () {
+    $(".slogan").typed({
+        strings : ["de Webdesign"," de Programmation"," et de toute solution IT"],
+        typespeed : 2,
+        loop : true,
+        backDelay : 4000,
+        showCursor: true,
+		// character for cursor
+        cursorChar: "|",   
+    });
+    /*$('.typedText').css({
+        'font-size':'18px',
+        'font-color':'black'
+    });*/
+    $(function blink(){
+        $('.typed-cursor').fadeOut(250).fadeIn(250, blink);
+    });
+    var sizeInit = $('.main-container').height();
 
     $('.bt_home').click(function () {
         $('.divToSlide').animate({
             left: "0vw"
         }, 1000, "easeInOutExpo");
+        var s = $('#section-home').height() - sizeInit + 50;
+        $('.main-container').css('height', s + 'px');
     });
     $('.bt_about').click(function () {
         $('.divToSlide').animate({
             left: "-100%"
         }, 1000, "easeInOutExpo");
+        var s = $('#section-aboutMe').height() - sizeInit + 50;
+        $('.main-container').css('height', s + 'px');
     });
     $('.bt_skill').click(function () {
         $('.divToSlide').animate({
@@ -19,6 +40,10 @@ $(function () {
         $('.divToSlide').animate({
             left: "-300%"
         }, 1000, "easeInOutExpo");
+        var expert_size = $('#section-experience').height();
+        var newSize = sizeInit + (expert_size - sizeInit);
+        $('.main-container').css('height', newSize + 50 + 'px');
+
     });
     $('.bt_portfolio').click(function () {
         $('.divToSlide').animate({
